ff52704a35a141a86dc97f3ba88862ab54c3328d
insider1405 <contact@promptix.dev>
2025-07-22 05:44:47 +0200

    Update Footer.tsx

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -78,7 +78,7 @@ export default function Footer({ className }: { className?: string }) {
     </React.Fragment>
   ));
 
-  const footerElements = [...mainContentRender, privacyPolicyRender, termsOfServiceRender].filter(
+  const footerElements = [...privacyPolicyRender, termsOfServiceRender].filter(
     Boolean,
   );
 
